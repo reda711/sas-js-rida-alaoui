77df6868a32f4bc7a82c55259ec28affb9788220
Reda711 <redaalaoui711@gmail.com>
2026-09-09 15:27:19 +0100

    exercice20completed

---
+++ day02/exercices/exercice-20.js
@@ -0,0 +1,26 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 02 · EXERCICE 20 · NIVEAU 3 : DÉFI (AVANCÉS)
+ * CHASSEUR DE NOMBRES D'ARMSTRONG
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Un nombre d'Armstrong à 3 chiffres est égal à la somme des cubes de ses chiffres (ex: 153 = 1³ + 5³ + 3³). Utilisez une boucle allant de 100 à 999 pour trouver et afficher TOUS les nombres d'Armstrong.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-20
+ * ▶️ Commande : node day02/exercices/exercice-20.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let chiffre = 153
+for (let i = 100 ; i <= 999 ; i++){
+    let firstdigit = Math.floor(i / 100) 
+    let seconddigit = Math.floor((i % 100) / 10)
+    let thirddigit = (i % 100) % 10 
+    let total = (firstdigit ** 3 ) + (seconddigit ** 3) + (thirddigit ** 3)
+    if (total === i){
+        console.log(`le nombre ${i} est un nombred'Armstrong`)
+    }
+    }
