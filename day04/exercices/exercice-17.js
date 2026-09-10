ea3c4cf3d51f18a55e0879c5d2805c87c54d177f
Reda711 <redaalaoui711@gmail.com>
2026-09-10 14:34:20 +0100

    ex17completed

---
+++ day04/exercices/exercice-17.js
@@ -0,0 +1,24 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 03 · EXERCICE 17 · NIVEAU 3 : DÉFI (AVANCÉS)
+ * LE NETTOYEUR DE DONNÉES SCRAPPÉES
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Créez une fonction extrairePrixEtConvertir(chaineBrute) qui reçoit, par exemple, "   Prix: 15.99 $  ". La fonction doit nettoyer la chaîne, isoler le chiffre, et retourner le type Number. Elle doit gérer les erreurs (retourner null si la chaîne ne contient pas de prix exploitable).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-17
+ * ▶️ Commande : node day03/exercices/exercice-17.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+function extrairePrixEtConvertir(chaineBrute) {
+    let cleantext = chaineBrute.replace(/[^\d.]/g, "");
+    cleantext = Number(cleantext)
+    if(cleantext === 0){
+        return null
+    }
+    return cleantext
+}
