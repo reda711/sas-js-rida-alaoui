433e8321190305ef00fbffdee31c4ff93bddb931
Reda711 <redaalaoui711@gmail.com>
2026-09-10 11:37:57 +0100

    ex12completed

---
+++ day04/exercices/exercice-12.js
@@ -0,0 +1,31 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 03 · EXERCICE 12 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * LE CALLBACK BASIQUE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Créez une fonction executerOperation(a, b, operation). operation sera une fonction passée en argument. Appelez : executerOperation(5, 3, multiplier) (en utilisant la fonction multiplier créée plus tôt).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-12
+ * ▶️ Commande : node day03/exercices/exercice-12.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+function executerOperation(a, b, operation) {
+    if (operation === "+"){
+        return a + b
+    }
+    
+    else if (operation == "-"){
+        return a - b
+    }
+    else if (operation == "*"){
+        return a * b
+    }
+    else if (operation == "/"){
+        return a / b
+    }
+}
