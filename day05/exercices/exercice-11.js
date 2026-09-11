d45034966338150b9e2aa35c8840e50c9f577e48
Reda711 <redaalaoui711@gmail.com>
2026-09-11 10:56:10 +0100

    ex11completed

---
+++ day05/exercices/exercice-11.js
@@ -0,0 +1,20 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 05 · EXERCICE 11 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * GÉNÉRATEUR D'ACRONYME
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * À partir de la phrase "Hyper Text Markup Language", retournez "HTML".
+ * Indice : Découpez en mots, bouclez, prenez la première lettre de chaque mot, mettez en majuscule, et rejoignez le tout.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-11
+ * ▶️ Commande : node day05/exercices/exercice-11.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let phrase = "Hyper Text Markup Language"
+    phrase = phrase.split(` `)
+    phrase = phrase[0].charAt(0).toUpperCase() + phrase[1].charAt(0).toUpperCase() + phrase[2].charAt(0).toUpperCase() + phrase[3].charAt(0).toUpperCase()
