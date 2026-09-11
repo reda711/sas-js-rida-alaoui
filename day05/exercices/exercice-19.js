345175424ad20a7c832277772e012a6525ee44ce
Reda711 <redaalaoui711@gmail.com>
2026-09-11 12:15:52 +0100

    ex19completed

---
+++ day05/exercices/exercice-19.js
@@ -0,0 +1,27 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 05 · EXERCICE 19 · NIVEAU 3 : DÉFI (AVANCÉS)
+ * VALIDATEUR DE PLAQUE D'IMMATRICULATION (REGEX BASIQUE)
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Simulez la vérification d'une plaque d'immatriculation marocaine. Le format attendu est "1234-A-56" ou "12345-AB-6". Pour simplifier, vérifiez qu'elle contient deux tirets et qu'une des sections au milieu est une lettre. L'utilisation d'expressions régulières (Regex) est recommandée ici !
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-19
+ * ▶️ Commande : node day05/exercices/exercice-19.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+function verifier(plaque){
+    const redex = /^[^-]+-[A-Za-z]+-[^-]+$/
+    return redex.test(plaque)
+    
+}
+
+console.log(verifier("1234-A-56"));
+console.log(verifier("1234-12-56"));
+console.log(verifier("12345-AB-6"));
+console.log(verifier("1234-A56"));
+
