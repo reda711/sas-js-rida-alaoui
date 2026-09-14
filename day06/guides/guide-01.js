362a1b5fb6bca92c1473ca671eee0a2170154bae
Reda711 <redaalaoui711@gmail.com>
2026-09-14 08:59:36 +0100

    guide01comnpleted

---
+++ day06/guides/guide-01.js
@@ -0,0 +1,15 @@
+/**
+ * JOUR 06 — GUIDE 01
+ * Accès et Modification
+ *
+ * OBJECTIF
+ * Créez un tableau fruits = ["Pomme", "Banane", "Orange"]. Changez "Banane" par "Mangue" et affichez le dernier élément du tableau.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let fruits = ["Pomme", "Banane", "Orange"]
+    fruits[1] = "Mangue"
+    console.log(fruits[fruits.length-1]);
+    
