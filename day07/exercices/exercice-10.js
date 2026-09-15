35f1f2cc5be7813b5aaf94a21ccea40025c5cbe9
Reda711 <redaalaoui711@gmail.com>
2026-09-15 09:21:12 +0100

    ex10comnpleted

---
+++ day07/exercices/exercice-10.js
@@ -0,0 +1,31 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 07 · EXERCICE 10 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * COMPTAGE DE VALEURS
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Vous avez une chaîne de caractères "abracadabra". Créez une fonction qui retourne un objet comptant le nombre d'occurrences de chaque lettre. (ex: { a: 5, b: 2, r: 2, c: 1, d: 1 }).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-10
+ * ▶️ Commande : node day07/exercices/exercice-10.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let magic = "abracadabra"
+function letters(phrase) {
+    let letterscount = {}
+    for (let i = 0; i < phrase.length; i++) {
+        if(letterscount[phrase[i]] !== undefined){
+            letterscount[phrase[i]]++
+        }        
+        else {
+            letterscount[phrase[i]] = 1
+        }
+    }
+    return letterscount
+}
+    console.log(letters(magic));
+    
