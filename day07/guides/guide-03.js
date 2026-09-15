e828326c4fe1f41099e04a449b1a795d37e2e01f
Reda711 <redaalaoui711@gmail.com>
2026-09-15 16:26:26 +0100

    guide03completed

---
+++ day07/guides/guide-03.js
@@ -0,0 +1,22 @@
+/**
+ * JOUR 07 — GUIDE 03
+ * Parcourir un Objet
+ *
+ * OBJECTIF
+ * Affichez toutes les clés et leurs valeurs de l'objet voiture sous la forme "clé : valeur".
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let car = {
+    marque : "Toyota",
+    modele : "Corolla",
+    annee : 2020
+}
+let keys = Object.keys(car)
+let values = Object.values(car)
+    for (let i = 0; i < keys.length; i++) {
+        console.log(keys[i] + ":" + values[i]);
+    
+}
