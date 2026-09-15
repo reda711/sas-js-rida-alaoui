c3ab7ab6a04ad823ed17646c3defa76b0dbb0dcc
Reda711 <redaalaoui711@gmail.com>
2026-09-15 16:10:30 +0100

    ex20comnpleted

--- day07/exercices/exercice-20.js
+++ day07/exercices/exercice-20.js
@@ -15,9 +15,16 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 let test = {
-    prenom : "Reda",
-    nom : "Alaoui"
-}
-let test2 = {...test}
-console.log(test2);
-log
+    prenom: "Reda",
+    nom: "Alaoui",
+    adresse: {
+        ville: "Meknes",
+        pays: "Maroc"
+    }
+};
+let clone = {...test}
+    clone.adresse.ville = "Midelt"
+console.log(clone);
+console.log(test);
+
+// Because after we clone an object we can change its properties without affecting the original but when we change the value on the nested object it affect the original also because it still reference to the same object
