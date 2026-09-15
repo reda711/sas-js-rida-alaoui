9710899b132b580f60124e8f48989da48b69d718
Reda711 <redaalaoui711@gmail.com>
2026-09-15 15:12:51 +0100

    ex19comnpleted

---
+++ day07/exercices/exercice-19.js
@@ -0,0 +1,34 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 07 · EXERCICE 19 · NIVEAU 3 : DÉFI (AVANCÉS)
+ * REGROUPEMENT (GROUP BY)
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+    * Vous avez un tableau [{nom: "A", groupe: 1}, {nom: "B", groupe: 2}, {nom: "C", groupe: 1}]. Créez une fonction qui retourne un objet regroupant ces données : { 1: [{nom: "A"}, {nom: "C"}], 2: [{nom: "B"}] }.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-19
+ * ▶️ Commande : node day07/exercices/exercice-19.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let tableau =  [{nom: "A", groupe: 1}, {nom: "B", groupe: 2}, {nom: "C", groupe: 1}]
+function regroup(array) {
+    let result = {}
+    for (let i = 0; i < array.length; i++) {
+        if(result[array[i].groupe] === undefined){
+            result[array[i].groupe] = [{
+                nom : array[i].nom
+            }]
+        
+        
+        }        
+        else {
+            result[array[i].groupe].push({nom : array[i].nom})
+        }
+    }
+    return result
+}
+console.log(regroup(tableau));
